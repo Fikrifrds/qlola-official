2f67bf47fd169228291ba476c4558e881eddaa9d
Fikri Firdaus <fikri.firdaus@rad4m.com>
2020-03-28 14:37:28 +0700

    update preloader

--- src/components/PreLoader/PreLoader.jsx
+++ src/components/PreLoader/PreLoader.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-export default () => {
-  const history = useHistory();
-  if (history) {
-    console.log(history);
-  }
-  return (
-    <div className="pre-loader">
-      <span className="spinner spinner-large spinner-blue spinner-slow" />
-    </div>
-  );
-};
+export default () => (
+  <div className="pre-loader">
+    <span className="spinner spinner-large spinner-blue spinner-slow" />
+  </div>
+);
