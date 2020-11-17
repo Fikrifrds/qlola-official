937142faae6cc5771c080426733a29d305095bde
Fikri Firdaus <fikri.firdaus26@gmail.com>
2020-11-17 11:22:32 +0700

    Update About.jsx

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -12,7 +12,7 @@ export default () => (
     <p>
       Email :
       {' '}
-      <a href="mailto:fikri@qlola.com?subject=Hello Fikri, Founder of Qlola" className="link link-bold">fikri@qlola.com</a>
+      <a href="mailto:qloladigital@gmail.com?subject=Hello Qlola" className="link link-bold">qloladigital@gmail.com</a>
     </p>
     <p className="about-description"><Text tid="companyDescription" /></p>
   </div>
